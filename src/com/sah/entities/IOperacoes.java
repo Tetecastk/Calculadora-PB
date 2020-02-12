d0d377b9841954f107799f3a966fe941810c741e
Stephanie Oliveira Chagas Freitas <stephanie.freitas@al.infnet.edu.br>
2020-02-11 21:09:43 -0300

    Refatoração de pasta e finalização da Calculadora 1.

---
+++ src/com/sah/entities/IOperacoes.java
@@ -0,0 +1,10 @@
+package com.sah.entities;
+
+public interface IOperacoes {
+
+	public double adicao(double num1 , double num2);
+	public double divisao(double num1 , double num2);
+	public double multiplicacao(double num1 , double num2);
+	public double subtracao(double num1 , double num2);
+	//public String mostrarMensagem(OperacaoService operacao);
+}
---
+++ src/com/sah/entities/OperacaoService.java
@@ -0,0 +1,46 @@
+package com.sah.entities;
+
+import javax.swing.JOptionPane;
+
+public class OperacaoService implements IOperacoes {
+
+	private double resultado = 0.0;
+	
+	
+	public double getResultado() {
+		return resultado;
+	}
+
+	@Override
+	public double adicao(double num1, double num2) {
+		resultado = num1 + num2;
+		return resultado;
+	}
+
+	@Override
+	public double divisao(double num1, double num2) {
+		try {
+			resultado = num1 / num2;
+			JOptionPane.showMessageDialog(null, num1 + " / " + num2 + " = " + resultado);
+		} catch (ArithmeticException  e) {
+			JOptionPane.showMessageDialog(null, "ERRO: Divisão por 0.");	
+		}
+		return resultado;
+	}
+
+	@Override
+	public double multiplicacao(double num1, double num2) {
+		resultado = num1 * num2;
+		
+		JOptionPane.showMessageDialog(null, num1 + " x " + num2 + " = " + resultado);
+		return resultado;
+	}
+
+	@Override
+	public double subtracao(double num1, double num2) {
+		resultado = num1 - num2;
+		
+		JOptionPane.showMessageDialog(null, num1 + " - " + num2 + " = " + resultado);
+		return resultado;
+	}
+}
